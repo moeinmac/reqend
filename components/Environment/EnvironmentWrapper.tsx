679cbad087f9baa8aa3b48937d78a2f461a2174c
moeinmac <moein.mac@gmail.com>
2025-11-09 20:21:02 +0330

    style : handle width of the envTable columns

--- components/Environment/EnvironmentWrapper.tsx
+++ components/Environment/EnvironmentWrapper.tsx
@@ -1,6 +1,7 @@
 import { useEnvStore } from "@/store/useEnvStore";
 import { FC, use } from "react";
 import { useShallow } from "zustand/react/shallow";
+import EnvTable from "./EnvTable";
 
 const EnvironmentWrapper: FC = () => {
   const { activeId, envs, changeAppMode } = useEnvStore(
@@ -12,6 +13,7 @@ const EnvironmentWrapper: FC = () => {
   return (
     <div className="flex flex-col gap-4 col-span-5 p-6">
       <h1 className="font-black text-4xl">Environment -::- {`${activeEnv ? activeEnv.name : "N/A"}`}</h1>
+      <EnvTable />
     </div>
   );
 };
