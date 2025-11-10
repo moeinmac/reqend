879cc818d5a24b79453ae2ac018faeb29253d6b0
moeinmac <moein.mac@gmail.com>
2025-11-10 22:25:20 +0330

    docs : add some descriptions for environment store functions

--- components/Environment/EnvironmentWrapper.tsx
+++ components/Environment/EnvironmentWrapper.tsx
@@ -11,6 +11,10 @@ const EnvironmentWrapper: FC = () => {
   return (
     <div className="flex flex-col gap-4 col-span-5 p-6">
       <h1 className="font-black text-4xl">Environment -::- {`${activeEnv ? activeEnv.name : "N/A"}`}</h1>
+      <p>
+        "They store config and secrets outside the API so the same app can behave differently in each environment and project — like giving it a new
+        costume for every show. {activeEnv?.id === "global" ? "(Global Environment can not be deleted or renamed)" : ""}"
+      </p>
       {activeEnv && <EnvTable />}
     </div>
   );
